2a3c0fe39b2b7f8b0530b602ffd07826af02430c
Kartik Sharma <Kartiksharma7808@gmail.com>
2025-06-18 13:23:12 +0530

    update index html file

--- usrFormDATABASE/src/main/java/com/example/demo/UsrFormDatabaseApplication.java
+++ usrFormDATABASE/src/main/java/com/example/demo/UsrFormDatabaseApplication.java
@@ -7,6 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsrFormDatabaseApplication {
 
 	public static void main(String[] args) {
+		
+		/*
+		User Authentication and Registration System
+			Backend: Spring Boot, Spring MVC, Spring Data JPA
+			Database: MySQL
+			Frontend (UI): Thymeleaf, bootstrap5
+			Tools Used: Eclipse IDE, Git & GitHub, MySQL Workbench, Apache Tomcat Server, Postman
+			
+			- Developed a secure web application to register users and authenticate login credentials.    
+			- Applied JPA and MySQL for seamless data persistence and retrieval.  
+			- Designed a mobile-responsive frontend with Bootstrap and custom CSS.
+			- Implemented role-based login and real-time feedback using toast messages and Thymeleaf.
+		*/
+		
 		System.out.println("This is UserForm Practice");
 		
 		SpringApplication.run(UsrFormDatabaseApplication.class, args);
